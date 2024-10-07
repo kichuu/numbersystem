b9817f0ce0c4031ad7adc54854cfcd34a81b8a78
krishnadev <krishnadevkichus@gmail.com>
2024-10-07 12:20:35 +0530

    added evenodd function

---
+++ index.js
@@ -0,0 +1,17 @@
+const numbers = [
+    3, 7, 2, 15, 9, 20, 5, 14, 12, 1, 8, 11, 6, 19, 4, 10, 17, 13, 16, 18,
+  ];
+
+  
+  function printEvenOdd(arr){
+    for(let i=0 ; i<arr.length ; i++){
+        if (arr[i] % 2 == 0){
+            console.log(arr[i])
+        }
+        if(arr[i] % 2 != 0 ){
+            console.log(arr[i])
+        }
+    }
+  }
+
+  printEvenOdd(numbers)
